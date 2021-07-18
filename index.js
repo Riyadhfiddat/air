f115b0b1a29f66b2065ac9299b78fd8a08a96869
MD.Abdullah Al Noman <56836018+Riyadhfiddat@users.noreply.github.com>
2021-07-18 14:08:52 +0600

    Commit Air

---
+++ index.js
@@ -0,0 +1,185 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+require('dotenv').config();
+
+const uri = process.env.DB_PATH;
+
+
+const MongoClient = require('mongodb').MongoClient;
+
+let   client = new MongoClient(uri, { useNewUrlParser: true });
+
+ app.post("/addHome", (req, res) => {
+  const homeRules = req.body;
+  client = new MongoClient(uri, { useNewUrlParser: true });
+  client.connect((error) => {
+    const collection = client.db("air-cnc").collection("homeRules");
+    collection.insert(homeRules, (err, result) => {
+      if (err) {
+        console.log(err);
+        console.log(error)
+        res.status(500).send({ message: err });
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
+    
+  });
+});
+
+app.get("/homeRules", (req, res) => {
+ 
+  client = new MongoClient(uri, { useNewUrlParser: true });
+  client.connect((error) => {
+    const collection = client.db("air-cnc").collection("homeRules");
+    collection.find().toArray((err, documents) => {
+      if (err) {
+        console.log(err);
+        console.log(error)
+        res.status(500).send({ message: err });
+      } else {
+        res.send(documents);
+      }
+    });
+    
+  });
+});
+
+ app.post("/addHome", (req, res) => {
+  const homeDetails = req.body;
+  const key = req.params.key;
+  const homeKeys = req.body;
+  client = new MongoClient(uri, { useNewUrlParser: true });
+  client.connect((error) => {
+    const collection = client.db("air-cnc").collection("homeDetails");
+    collection.insert(homeDetails, (err, result) => {
+      if (err) {
+        console.log(err);
+        console.log(error)
+        res.status(500).send({ message: err });
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
+    
+  });
+});
+
+app.post('/homeDetails', (req, res) =>{
+  const key = req.params.key;
+  const homeKeys = req.body;
+  client = new MongoClient(uri, { useNewUrlParser: true });
+  client.connect(err => {
+      const collection = client.db("air-cnc").collection("homeDetails");
+      collection.find({key: { $in: homeKeys }}).toArray((err, documents)=>{
+          if(err){
+              console.log(err)
+              res.status(500).send({message:err});
+          }
+          else{
+              res.send(documents);
+          }
+      });
+     
+    });
+});
+app.get('/homeDetails/:key', (req, res) =>{
+  const key = req.params.key;    
+  client = new MongoClient(uri, { useNewUrlParser: true });
+  client.connect(err => {
+      const collection = client.db("air-cnc").collection("homeDetails");
+      collection.find({key}).toArray((err, documents)=>{
+          if(err){
+              console.log(err)
+              res.status(500).send({message:err});
+          }
+          else{
+              res.send(documents[0]);
+          }
+      });
+      
+    });
+});
+
+ app.post("/addHome", (req, res) => {
+    const homes = req.body;
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect((error) => {
+      const collection = client.db("air-cnc").collection("homes");
+      collection.insert(homes, (err, result) => {
+        if (err) {
+          console.log(err);
+          console.log(error)
+          res.status(500).send({ message: err });
+        } else {
+          res.send(result.ops[0]);
+        }
+      });
+     
+    });
+  });
+
+  app.get("/homes", (req, res) => {
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect((error) => {
+      const collection = client.db("air-cnc").collection("homes");
+      collection.find().toArray((err, documents) => {
+        if (err) {
+          console.log(err);
+          console.log(error)
+          res.status(500).send({ message: err });
+        } else {
+          res.send(documents);
+        }
+      });
+    });
+  });
+
+
+
+app.post("/addHome", (req, res) => {
+    const experiences = req.body;
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect((error) => {
+      const collection = client.db("air-cnc").collection("experiences");
+      collection.insert(experiences, (err, result) => {
+        if (err) {
+          console.log(err);
+          console.log(error)
+          res.status(500).send({ message: err });
+        } else {
+          res.send(result.ops[0]);
+        }
+      });
+     
+    });
+  });
+  app.get("/experiences", (req, res) => {
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect((error) => {
+      const collection = client.db("air-cnc").collection("experiences");
+      collection.find().toArray((err, documents) => {
+        if (err) {
+          console.log(err);
+          console.log(error)
+          res.status(500).send({ message: err });
+        } else {
+          res.send(documents);
+        }
+      });
+    });
+  });
+
+
+  app.get("/", (req, res) => {
+    res.send("<h1>air-cnc Server</h1>");
+  });
+
+const port = process.env.PORT || 4200;
+app.listen(port, () => { console.log("Listening at port 4200");
+});
